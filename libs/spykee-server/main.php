d17b6e6e8f9acc6ebcb458c475ca7a7198381ea0
shadow_man <rem730@gmail.com>
2013-04-14 00:32:55 +0200

    Second essay de changement de retour à la ligne et d'encodage

--- libs/spykee-server/main.php
+++ libs/spykee-server/main.php
@@ -4,187 +4,187 @@
 set_time_limit(0);
 
 class SpykeeServer{
-	private static $_noRobot = 0; 
-	
+	private static $_noRobot = 0;
+
 	/*
 	 * Actions
-	 */
+	*/
 	const TURNLEFT = 1;
 	const TURNRIGHT = 2;
-	
+
 	/*
 	 * Etats de l'action
-	 */
+	*/
 	const STATEOK = 0;
 	const STATEERROR = 1;
-	
+
 	/*
 	 * Configuration
-	 */
+	*/
 	const TIMEZONE = 'Europe/Paris';
 	const MAXCONNECTION = 10;
 	const FIRSTPORT = 2000; // Numero de port du premier server
 	const SERVEURIP = '127.0.0.1';
-	const CLIENTIP = '127.0.0.1'; // UtilisÈ lors du flitrage des trames
-	
+	const CLIENTIP = '127.0.0.1'; // Utilis√© lors du flitrage des trames
+
 	/*
-	 * DÈfinition des attributs
-	 */
+	 * D√©finition des attributs
+	*/
 	private $_serverPort;
 	private $_stopServer = false;
 	private $_robotName;
 	private $_robotIp;
 	private $_logFile;
-	
+
 	function __construct($robotName, $robotIp, $serverPort=''){
 		self::$_noRobot++;
 		// TODO verifier que les ports sont disponibles
 		$this->$_serverPort = ($serverPort=='') ? self::FIRSTPORT + self::$_noRobot : $serverPort;
-		// TODO vÈrifier les valeurs entrÈes avec un geter
+		// TODO v√©rifier les valeurs entr√©es avec un geter
 		$this->_logFile = realpath(__FILE__).'../../logs/'.$name.'.log';
 		$this->_robotName = $robotName;
 		$this->_robotIp = $robotIp;
 		date_default_timezone_set(self::TIMEZONE);
-		
+
 		$this->connectionToTheRobot();
-		
+
 		$this->listenNetwork();
 	}
-	
+
 	private function connectionToTheRobot(){
-		
+
 	}
-	
+
 	private function writeLog($txt){
-		$content = date('[d/m/y] ‡ H:i:s ', time());
+		$content = date('[d/m/y] √† H:i:s ', time());
 		$content .= '@'.$this->_robotName.'('.$this->_robotIp.') ';
 		$content .= $txt;
-		
+
 		/*
-		 * CrÈation/Mise ‡ jour du fichier de log
-		 */
+		 * Cr√©ation/Mise √† jour du fichier de log
+		*/
 		if (!$file = fopen($this->_logFile, 'a')){
 			echo 'Impossible d\'ouvrir le fichier : '.$this->_logFile;
 		}
 		else {
 			if (fwrite($file, $content) === FALSE ){
-				echo 'Impossible d\'Ècrire dans le fichier : '.$this->_logFile;
+				echo 'Impossible d\'√©crire dans le fichier : '.$this->_logFile;
 			}
 			fclose($file);
 		}
 			
 	}
-	
+
 	private function listenNetwork(){
 		// Source : http://www.binarytides.com/php-socket-programming-tutorial/
-		
+
 		if(!($sock = socket_create(AF_INET, SOCK_STREAM, 0)))
 		{
 			$errorcode = socket_last_error();
 			$errormsg = socket_strerror($errorcode);
-			 
-			$this->writeLog('Impossible de crÈer le socket : ['.$errorcode.'] '.$errormsg."\n");
+
+			$this->writeLog('Impossible de cr√©er le socket : ['.$errorcode.'] '.$errormsg."\n");
 			die;
 		}
-		
+
 		//echo "Socket created \n";
-		
+
 		// Bind the source address
 		if( !socket_bind($sock, self::SERVEURIP , $this->$_serverPort) )
 		{
 			$errorcode = socket_last_error();
 			$errormsg = socket_strerror($errorcode);
-			 
+
 			$this->writeLog('Impossible de lier le socket : ['.$errorcode.'] '.$errormsg."\n");
 			die;
 		}
-		
+
 		//echo "Socket bind OK \n";
-		
+
 		if(!socket_listen ($sock , self::MAXCONNECTION))
 		{
 			$errorcode = socket_last_error();
 			$errormsg = socket_strerror($errorcode);
-			 
-			$this->writeLog('Impossible d\'Ècouter le socket : ['.$errorcode.'] '.$errormsg."\n");
+
+			$this->writeLog('Impossible d\'√©couter le socket : ['.$errorcode.'] '.$errormsg."\n");
 			die;
 		}
-		
+
 		//echo "Socket listen OK \n";
-		
+
 		//echo "Waiting for incoming connections... \n";
-		
+
 		//array of client sockets
 		$client_socks = array();
-		
+
 		//array of sockets to read
 		$read = array();
-		
+
 		//start loop to listen for incoming connections and process existing connections
 		while (!$this->_stopServer)
 		{
 			//prepare array of readable client sockets
 			$read = array();
-			 
+
 			//first socket is the master socket
 			$read[0] = $sock;
-			 
+
 			//now add the existing client sockets
 			for ($i = 0; $i < self::MAXCONNECTION; $i++)
 			{
 				if($client_socks[$i] != null){
-				$read[$i+1] = $client_socks[$i];
+					$read[$i+1] = $client_socks[$i];
 				}
 			}
-			 
+
 			//now call select - blocking call
-			// TODO $write n'est pas utilise, ‡ supprimer
+			// TODO $write n'est pas utilise, √† supprimer
 			if(socket_select($read , $write , $except , null) === false){
 				$errorcode = socket_last_error();
 				$errormsg = socket_strerror($errorcode);
-				 
-				$this->writeLog('Impossible d\'Ècouter le socket : ['.$errorcode.'] '.$errormsg."\n");
+					
+				$this->writeLog('Impossible d\'√©couter le socket : ['.$errorcode.'] '.$errormsg."\n");
 				die;
 			}
-			 
+
 			//if read contains the master socket, then a new connection has come in
 			if (in_array($sock, $read)){
 				for ($i = 0; $i < self::MAXCONNECTION; $i++){
 					if ($client_socks[$i] == null){
 						$client_socks[$i] = socket_accept($sock);
 						/*
-						 * Code exÈcutÈ lors de la connexion entre le client et le serveur
-						 */
-						
+						 * Code ex√©cut√© lors de la connexion entre le client et le serveur
+						*/
+
 						// Filtrage IP
 						socket_getpeername($client_socks[$i], $clientIp);
 						if ($clientIp != self::CLIENTIP){
 							unset($client_socks[$i]);
 							socket_close($client_socks[$i]);
 						}
-						
-						// TODO Connexion TCP -> Session crÈe. On as besoin d'envoyer des donnÈes pour confirmer la connexion ?
+
+						// TODO Connexion TCP -> Session cr√©e. On as besoin d'envoyer des donn√©es pour confirmer la connexion ?
 						break;
 					}
 				}
 			}
-		
+
 			//check each client if they send any data
 			for ($i = 0; $i < self::MAXCONNECTION; $i++){
 				if (in_array($client_socks[$i] , $read)){
 					/*
-					 * Code exÈcutÈ 
-					 */
+					 * Code ex√©cut√©
+					*/
 					$input = socket_read($client_socks[$i] , 1024);
-       
-            		// Suppression de la Session(connexion)
-            		if ($input == null){
+
+					// Suppression de la Session(connexion)
+					if ($input == null){
 						//zero length string meaning disconnected, remove and close the socket
 						unset($client_socks[$i]);
 						socket_close($client_socks[$i]);
 					}
-		
-					// Fait l'action demandÈ
+
+					// Fait l'action demand√©
 					switch($input){
 						case self::TURNLEFT:
 							$state = $this->turnLeft();
@@ -195,27 +195,27 @@ class SpykeeServer{
 						default:
 							$state = self::STATEERROR;
 					}
-			 
+
 					//send response to client
 					socket_write($client_socks[$i] , $state);
 				}
 			}
-		}		
+		}
 	}
-	
+
 	private function turnLeft(){
-		
+
 		return $state;
 	}
-	
+
 	private function turnRight(){
-	
+
 		return $state;
 	}
-	
+
 	function __destruct(){
 		self::$_noRobot--;
-	}	
+	}
 }
 
 ?>
